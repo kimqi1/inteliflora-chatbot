17762442ff4c9830932d7d42c2ea0e575478c45d
sufiyanirfan3 <sufiyanirfan1613@gmail.com>
2024-03-21 03:00:20 +0500

    update option

--- app/components/Option.tsx
+++ app/components/Option.tsx
@@ -2,7 +2,7 @@ import { IOptionProps } from "../../types";
 
 const Option: React.FC<IOptionProps> = ({ onClick, children }) => (
   <button
-    className="border border-green hover:bg-green text-green hover:text-white py-2 px-4 rounded my-2 mr-2"
+    className="text-sm border border-green hover:bg-green text-green hover:text-white p-2 rounded my-2 mr-2"
     onClick={onClick}
   >
     {children}
