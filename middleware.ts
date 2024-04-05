a6fbf166a51496aa26ca7d94908eba60baf4b200
sufiyanirfan3 <sufiyanirfan1613@gmail.com>
2024-04-06 02:36:00 +0500

    fixes

---
+++ middleware.ts
@@ -0,0 +1,11 @@
+import { NextResponse, NextRequest } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const response = NextResponse.next();
+  // Set custom headers or perform other middleware logic here
+  response.headers.set(
+    "Content-Security-Policy",
+    "frame-ancestors 'self' https://inteliflora.com/"
+  );
+  return response;
+}
