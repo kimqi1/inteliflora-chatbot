5e68beb101bad9203e81361086ee801bc271d0b4
sufiyanirfan3 <sufiyanirfan1613@gmail.com>
2024-04-06 03:50:59 +0500

    further security

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,31 @@
-import type { Metadata } from "next";
+// First line of your component file
+"use client";
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
-
+import { useEffect } from "react";
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Inteliflora",
-  description: "Inteliflora chatbot",
-};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  useEffect(() => {
+    if (window.top === window.self) {
+      // The page is not in an iframe
+      document.body.innerHTML = `
+      <div style="display: flex; flex-direction:column; justify-content: center; align-items: center; height: 100vh; font-size: 24px; font-family: Arial, sans-serif;">
+      <p>404</p>
+        <p>Website Not Found<p>
+      </div>
+    `;
+    }
+  }, []); // Empty dependency array means this runs once on component mount
+
   return (
     <html lang="en">
-      <head>
-        {/* Other head elements */}
-        <script dangerouslySetInnerHTML={{ 
-          __html: `
-            if (window.top === window.self) {
-              // The page is not in an iframe
-              document.body.innerHTML = 'This content is only accessible through an iframe.';
-            }
-          `
-        }}></script>
-      </head>
       <body className={inter.className}>
         {children}
         <Toaster />
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 import ChatContainer from "./components/ChatContainer";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Inteliflora",
+  description: "Inteliflora chatbot",
+};
+
 
 export default function Home() {
   return (
+    <>
     <main className="flex flex-col items-center justify-center w-full min-h-screen px-2 py-4 sm:p-4 gap-4 md:flex-row md:p-8">
       <div className="w-full max-w-5xl mx-auto">
         <div className="card bg-white p-0 sm:p-2 border border-gray-200 rounded-lg h-[85vh]">
@@ -9,5 +17,6 @@ export default function Home() {
         </div>
       </div>
     </main>
+    </>
   );
 }
